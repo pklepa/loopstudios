6578775925f3a659d7a7fadddc02e76ed012f7e6
Pedro Klepa <pedro.klepa@gmail.com>
2021-01-26 12:06:45 +0000

    Gatsby-image works

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,14 @@
 import React from "react"
+
+import HeroBackground from "../components/heroBackground"
 import Navbar from "../components/navbar"
 import Title from "../components/title"
 
 export default function Home() {
   return (
     <div>
+      <HeroBackground />
+
       <Navbar />
       <Title />
     </div>
